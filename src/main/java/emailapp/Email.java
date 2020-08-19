b8ee7b4752ae362a1d5e5d16ecb1a308c2b59232
Kendyl Hull <hullkendyl@gmail.com>
2020-08-19 17:28:41 -0400

    created a method asking for the employee department and returning the selected department

--- src/main/java/emailapp/Email.java
+++ src/main/java/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 
     private String firstName;
@@ -13,5 +15,19 @@ public class Email {
     public Email(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+        System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+    }
+
+    private String setDepartment() {
+        System.out.println("Enter the department\n1 for Administrative\n2 for Dietary\n3 for Nursing\n0 for none");
+        Scanner scanner = new Scanner(System.in);
+        int departmentChoice = scanner.nextInt();
+        if (departmentChoice == 1) {
+            return "Administrative";}
+        else if (departmentChoice == 2) {
+            return "Dietary";}
+        else if (departmentChoice == 3) {
+            return "Nursing"; }
+        else return " ";
     }
 }
